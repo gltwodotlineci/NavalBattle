efa741c6b58b7ab8e76f6bc95f5cbe9259c777f5
glen <glen84_dr@hotmail.com>
2023-01-14 23:35:56 +0400

    begining game

---
+++ grid.py
@@ -0,0 +1,6 @@
+class Grid:
+    def __init__(self, width):
+        self.width = width
+
+    def create_grid(self):
+        pass
---
+++ ship.py
@@ -0,0 +1,8 @@
+class Ship:
+    def __init__(self, grid_dim, start_point, end_point):
+            self.grid_dim = grid_dim
+            self.start_point = start_point
+            self.end_point = end_point
+
+    def creat_ship(self):
+        pass
