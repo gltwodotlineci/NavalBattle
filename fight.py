2420812bddfe641f9b64ce00667589b2f18571cf
glen <glen84_dr@hotmail.com>
2023-01-19 00:16:00 +0400

    Attack level game

--- fight.py
+++ fight.py
@@ -5,3 +5,35 @@ class Fighting:
         self.gamer1 = gamer1
         self.gamer2 = gamer2
         self.dimention = dimention
+        self.missile_coordinate = []
+        self.succes = 1
+
+    def check_win(self,contest1, contest2):
+        Attack(contest1.grid, self.missile_coordinate, self.dimention).result_attack()
+        self.succes = 0
+        self.missile_coordinate = []
+        for r in contest1.grid:
+            for cel in r:
+                if cel == "[o]":
+                    self.succes = self.succes + 1
+        if self.succes == 0:
+            while 1:
+                if self.succes == 0:
+                    print("*********************")
+                    print(" ")
+                    print("CONGRATS "+contest2.name+" YOU WON!!!")
+                    print(" ")
+                    print("*********************")
+                    break
+
+    def fighting(self):
+        print("The gamer One had the advantage of choosing the game dimention.\n So, the gamer 2 will star the attack")
+        while self.succes > 0:
+            for index, gamer in enumerate([self.gamer2, self.gamer1]):
+                for j in range(1,3):
+                    ele = int(input(gamer.name+" Choose the coordinate "+str(j)+" of your missile attack "))
+                    self.missile_coordinate.append(ele)
+                if index == 0:
+                    self.check_win(self.gamer1, self.gamer2)
+                else:
+                    self.check_win(self.gamer2, self.gamer1)
--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 from ship import Ship
 from grid import Grid
+from game import Game
+from player import Player
+from fight import Fighting
 
 
 def create_ships():
     global ship_start, ship_end
     ship_start, ship_end = [], []
+
+    for player in [player1, player2]:
+        print("_____________________")
+        print("Hi "+player.name+" you can start creating your three ships")
+        print("_____________________")
+        for n in range(1,2):
+            for i in range(0,2):
+                if i == 0:
+                    ship_start.append(int(input("Choose the 1st coordinate for the start of ship "+str(n)+" ")))
+                    ship_start.append(int(input("Choose the second coordinate for the start of ship "+str(n)+" ")))
+                elif i == 1:
+                    ship_end.append(int(input("Choose the 1st coordinate for the end of ship "+str(n)+" ")))
+                    ship_end.append(int(input("Choose the second coordinate for the end of ship "+str(n)+" ")))
+
+            Ship(player.grid, ship_start[0:2], ship_end[0:2]).creat_ship()
+            ship_start, ship_end = [], []
+
+creat_ships()
+
+Fighting(player1, player2, dimention_game).fighting()
