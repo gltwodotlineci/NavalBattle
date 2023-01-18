842b5df3c7a18a34926d3455e83f1a76f0d278a3
glen <glen84_dr@hotmail.com>
2023-01-18 23:56:12 +0400

    add Game and Attack classes

---
+++ fight.py
@@ -0,0 +1,7 @@
+from attack import Attack
+
+class Fighting:
+    def __init__(self, gamer1, gamer2, dimention):
+        self.gamer1 = gamer1
+        self.gamer2 = gamer2
+        self.dimention = dimention
---
+++ game.py
@@ -0,0 +1,36 @@
+
+class Game:
+
+    def start_game():    
+        global name, name2, choice
+
+        print("Hi, welcom to Ship batle game, please choose your name and the dimention of the game!")
+        name = input("What is you name player 1: ")
+        dimention = input(" Welcome "+name+", you're the first player, the first player have the right to chose the dimention of the game \n what's the dimention of the game that you want to play? \n press 1 if you want a 8 x 8 dimention game \n press 2 if you want a  16 x 16 game\n press 3 if you want a  26 x 26 game\n ")
+        choice = int(dimention)
+
+        while choice > 3:
+            input2 = input(" Wrong choice. you can only press 1, 2 or 3. \n press 1 if you want a 8 x 8 dimention game \n press 2 if you want a  16 x 16 game \n press 3 if you want a  26 x 26 game\n ")
+            choice = int(input2)
+
+        print("Now the second player can write his hame and if he accept the dimention of the game proposed from the player")
+        print('___________________________')
+        name2 = input("Hello the player 2, what's your name? ")
+        accept_dim = input("Welcome "+name2+", if you accept the dimention type 'Yes' ")
+        if accept_dim != "Yes":
+            return False
+
+
+    def choosing():
+        match choice:
+            case 1:
+                return 8
+            case 2:
+                return 16
+            case 3:
+                return 26
+
+    def name_players():
+        return [name, name2]
+
+
