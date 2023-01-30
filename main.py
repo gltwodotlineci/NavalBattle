bb9b17b928684ec3d80cf7d21e71c0f5f0aa5a87
glen <glen84_dr@hotmail.com>
2023-01-31 00:14:10 +0400

    refacto ship

--- main.py
+++ main.py
@@ -16,33 +16,6 @@ Game.show_game_board(dimention_game)
 player1 = Player(Game.name_players()[0], Grid(dimention_game).create_grid())
 player2 = Player(Game.name_players()[1], Grid(dimention_game).create_grid())
 
-
-def create_ships():
-    global ship_start, ship_end
-    ship_start, ship_end = [], []
-
-    for player in [player1, player2]:
-        print("_____________________")
-        print("Hi "+player.name+" you can start creating your three ships")
-        print("_____________________")
-        for n in range(1,4):
-            for i in range(0,2):
-                if i == 0:
-                    begining_ship_x =  InteractConditions(player.name, n).coordinate_conditions(dimention_game, "of the begining of yur ship "+str(n)+": ")
-                    begining_ship_y = InteractConditions(player.name, n).coordinate_conditions(dimention_game, "of the begingin of yur ship "+str(n)+": ")
-                else:
-                    end_ship_x =  InteractConditions(player.name, n).coordinate_conditions(dimention_game, "of the end of yur ship "+str(n)+": ")
-                    end_ship_x = InteractConditions(player.name, n).verticla_length(begining_ship_x, end_ship_x)
-                    end_ship_y = InteractConditions(player.name, n).coordinate_conditions(dimention_game,  "of the end of yur ship "+str(n)+": ")
-                    end_ship_y = InteractConditions(player.name, n).ship_conditions(begining_ship_x, begining_ship_y, end_ship_x, end_ship_y)
-            ship_start.append(begining_ship_x)
-            ship_start.append(begining_ship_y)
-            ship_end.append(end_ship_x)
-            ship_end.append(end_ship_y)
-
-            Ship(player.grid, ship_start[0:2], ship_end[0:2]).creat_ship()
-            ship_start, ship_end = [], []
-
-create_ships()
+Ship(player1, player2).create_ships(dimention_game)
 
 Fighting(player1, player2, dimention_game).player_hit()
--- ship.py
+++ ship.py
@@ -1,15 +1,43 @@
-class Ship:
-    def __init__(self, grid, start_point, end_point):
-            self.grid = grid
-            self.start_point = start_point
-            self.end_point = end_point
+from interact_conditions import InteractConditions
 
-    def creat_ship(self):
-        if self.start_point[0] == self.end_point[0]:
-            for j in range(self.start_point[1],(self.end_point[1]+1)):
-                self.grid[self.start_point[0]][j] = "[o]"
-        elif self.start_point[1] == self.end_point[1]:
-            for k in range((self.start_point[0]),self.end_point[0]+1):
-                self.grid[k][self.end_point[1]] = "[o]"
+class Ship:
+    def __init__(self, player1, player2):
+            self.player1 = player1
+            self.player2 = player2
+            self.ship_start, self.ship_end  = [], []
+
+
+    def creat_ship(self, grid):
+        if self.ship_start[0] == self.ship_end[0]:
+            for j in range(self.ship_start[1],(self.ship_end[1]+1)):
+                grid[self.ship_start[0]][j] = "[o]"
+        elif self.ship_start[1] == self.ship_end[1]:
+            for k in range((self.ship_start[0]),self.ship_end[0]+1):
+                grid[k][self.ship_end[1]] = "[o]"
         print("____________________________________")
-        return self.grid
+        return grid
+
+
+    def create_ships(self, dimention_game):#, player1, player2, dimention_game):
+
+        for player in [self.player1, self.player2]:
+            print("_____________________")
+            print("Hi "+player.name+" you can start creating your three ships")
+            print("_____________________")
+            for n in range(1,4):
+                for i in range(0,2):
+                    if i == 0:
+                        begining_ship_x =  InteractConditions(player.name, n).coordinate_conditions(dimention_game, "of the begining of yur ship "+str(n)+": ")
+                        begining_ship_y = InteractConditions(player.name, n).coordinate_conditions(dimention_game, "of the begingin of yur ship "+str(n)+": ")
+                    else:
+                        end_ship_x =  InteractConditions(player.name, n).coordinate_conditions(dimention_game, "of the end of yur ship "+str(n)+": ")
+                        end_ship_x = InteractConditions(player.name, n).verticla_length(begining_ship_x, end_ship_x)
+                        end_ship_y = InteractConditions(player.name, n).coordinate_conditions(dimention_game,  "of the end of yur ship "+str(n)+": ")
+                        end_ship_y = InteractConditions(player.name, n).ship_conditions(begining_ship_x, begining_ship_y, end_ship_x, end_ship_y)
+                self.ship_start.append(begining_ship_x)
+                self.ship_start.append(begining_ship_y)
+                self.ship_end.append(end_ship_x)
+                self.ship_end.append(end_ship_y)
+
+                self.creat_ship(player.grid)
+                self.ship_start, self.ship_end = [], []
